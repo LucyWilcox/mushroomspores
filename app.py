7a2f2b6ba6f9acec7f3a3271111a7208fc40ac0d
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2017-04-06 16:49:39 +0200

    remove next

--- app.py
+++ app.py
@@ -134,8 +134,8 @@ def post_url():
 		print "could not find request.json"
 	if not 'currurl' in request.json:
 		print "no currurl in request.json"
-	if not 'nexturl' in request.json:
-		print "no nexturl in request.json"
+	# if not 'nexturl' in request.json:
+	# 	print "no nexturl in request.json"
 	print request.json
 	conn = psycopg2.connect(database_URI)
 	curr = conn.cursor()
@@ -143,11 +143,11 @@ def post_url():
 	curr_url = "'" + curr_url + "'"
 	sql = "UPDATE CurrentId SET curr_url = %s WHERE id = 1" %(curr_url)
 	curr.execute(sql)
-	next_url = request.json['nexturl']
-	next_url = "'" + next_url + "'"
-	sql = "UPDATE CurrentId SET next_url = %s WHERE id = 1" %(next_url)
-	print sql
-	curr.execute(sql)
+	# next_url = request.json['nexturl']
+	# next_url = "'" + next_url + "'"
+	# sql = "UPDATE CurrentId SET next_url = %s WHERE id = 1" %(next_url)
+	# print sql
+	# curr.execute(sql)
 	conn.commit()
 	curr.close()
 	conn.close()
